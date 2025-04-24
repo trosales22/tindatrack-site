0174d6d86e4f45250ee6423c17cd2af6f8cafd61
Tristan Rosales <tristanrosales0@gmail.com>
2025-04-24 23:24:41 +0800

    Remove navbar-start content in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { Menu } from "lucide-react";
 
 const Header: React.FC = () => {
   return (
     <div className="navbar fixed top-0 bg-base-100 shadow-sm z-50">
       <div className="navbar-start">
-        <div className="dropdown">
-          <button tabIndex={0} className="btn btn-ghost btn-circle">
-            <Menu className="w-6 h-6" />
-          </button>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li><a href="#home">Home</a></li>
-            <li><a href="#features">Features</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
       </div>
 
       <div className="navbar-center flex items-center gap-2">
